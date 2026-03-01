a756ff3009bcfdadaa7ad00a49a6643fe3556882
nahua3730 <nahua3730@gmail.com>
2026-02-28 22:44:42 -0500

    Add admin view for most commented user

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('most-commented/', views.most_commented_user, name='most_commented'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Count, Max
+from movies.models import Review
 def index(request):
     template_data = {}
     template_data['title'] = 'Movies Store'
@@ -7,3 +9,15 @@ def about(request):
     template_data = {}
     template_data['title'] = 'About'
     return render(request, 'home/about.html', {'template_data': template_data})
+def most_commented_user(request):
+    if not request.user.is_staff:
+        return redirect('home.index')
+    counts = Review.objects.values('user__username').annotate(total=Count('id'))
+    max_total = counts.aggregate(max_total=Max('total'))['max_total']
+    top_users = []
+    if max_total is not None:
+        top_users = counts.filter(total=max_total).order_by('user__username')
+    return render(request, 'home/most_commented_user.html', {
+        'max_total': max_total,
+        'top_users': top_users,
+    })
