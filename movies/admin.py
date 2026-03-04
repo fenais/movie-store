b1b8d9587374d75656f12f27cec7c2b989df8be5
natalieseng <natalieseng72@gmail.com>
2026-03-03 21:20:48 -0500

    Added User Story 3 function and added visual seperate admin tab dashboard for UserStory5 and UserStory3

--- movies/admin.py
+++ movies/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.template.response import TemplateResponse
 from django.urls import path
 from django.db.models import Count, Sum
+from django.contrib.auth.models import User
+from django.db.models.functions import Coalesce
 
 from .models import Movie, Review
 from cart.models import Item
@@ -38,13 +40,23 @@ def movie_stats_view(request):
         most_reviewed=most_reviewed,
         most_bought=most_bought,
     )
-    return TemplateResponse(request, "movies/admin/movie_stats.html", context)
+    return TemplateResponse(request, "admin/movie_stats.html", context)
+
+def user_stats_view(request):
+    top_buyer = User.objects.annotate(total_movies=Coalesce(Sum('order__item__quantity'), 0)).order_by('-total_movies').first()
+    context = dict(
+        admin.site.each_context(request),
+        title="Top Customer",
+        top_buyer= top_buyer,
+    )
+    return TemplateResponse(request, "admin/user_stats.html", context)
 
 original_get_urls = admin.site.get_urls
 def get_urls():
     urls = original_get_urls() #the fix
     custom_urls =[
         path("movie-stats/", admin.site.admin_view(movie_stats_view), name="movie-stats"),
+        path("user-stats/", admin.site.admin_view(user_stats_view), name="user-stats"),
     ]
     return custom_urls + urls
 
